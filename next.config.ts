e6b576640e3c1d50503ec9ea8b9e6a0c0eb119f1
wira <wirabkti@gmail.com>
2025-07-03 00:31:28 +0700

    git commit -m "feat: add other config"

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.escuelajs.co",
+        port: "",
+        pathname: "/**",
+      },
+      // Tambahkan domain lain jika diperlukan
+    ],
+  },
 };
 
 export default nextConfig;
